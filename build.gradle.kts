b992d91551de87433554dfd79a739d59062c9f7a
Amos Lim <amoseui@gmail.com>
2020-03-10 23:34:09 +0900

    Create the project with GitHub Actions (#2)
    Close #1

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    val kotlinVersion = "1.3.61"
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.6.1")
+        classpath(kotlin("gradle-plugin", version = kotlinVersion))
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "ypc"
+include(":app")
